c589ef45cae934ba88bac62136bdd3544bbbe190
DaedalusCommunity <53686789+DaedalusCommunity@users.noreply.github.com>
2019-11-15 18:51:02 +0100

    There might be something weird in the Orbit Script
    Because I'm still working on it, but like ignore the things I haven't been talking about in the videos, I'll cover them in the next parts

---
+++ Orbit.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject orb;
+    public float a;
+    public float b;
+    public float c;
+    [SerializeField] float alpha;
+    [SerializeField] float deltaAlpha;
+    public Vector3 center;
+    public Transform focus1;
+    [SerializeField] float G;
+    [SerializeField] float M;
+    float r;
+    Rigidbody rb;
+    [SerializeField] Vector3 Velocity;
+    [SerializeField] float speed;
+    [SerializeField] float XZAngle;
+    [SerializeField] float XZGrad;
+    [SerializeField] float tanXZ;
+    Vector3 periapsis;
+    Vector3 apoapsis;
+    public float beta;
+    public float ArgOfPer;
+    VisualizeOrbit VO;
+
+    public float gamma;
+    public float delta;
+
+    public TextMesh tm;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+        //VO = orb.GetComponent<VisualizeOrbit>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*periapsis = VO.periapsis;
+        apoapsis = VO.apoapsis;*/
+
+       
+       
+
+        MoveObject();
+        speed = Mathf.Sqrt(G * M * ((2 / r) - (1 / a)));
+        Vector3 backRot = Quaternion.Euler(-gamma, -beta, -delta) * transform.position;
+
+        XZAngle = Mathf.Atan(-((b * (backRot.x - center.x)) / (2 * a * Mathf.Sqrt(a * a - (backRot.x - center.x) * (backRot.x - center.x)))));
+        XZGrad = XZAngle * Mathf.Rad2Deg;
+        tanXZ = -((b * (backRot.x - center.x)) / (2 * a * Mathf.Sqrt(a * a - (backRot.x - center.x) * (backRot.x - center.x))));
+        Velocity = new Vector3(speed * Mathf.Cos(XZAngle),0 ,speed * Mathf.Sin(XZAngle));
+        //Debug.DrawLine(transform.position, transform.position + Velocity*10000);
+        //tm.text = XZGrad.ToString();
+    }
+    void CalculateVelocity()
+    {
+
+    }
+
+    void MoveObject()
+    {
+        if (a >= b)
+        {
+            c = Mathf.Sqrt(Mathf.Abs(a * a - b * b));
+            center = new Vector3(focus1.position.x + c, 0, focus1.position.z);
+
+            r = Vector3.Distance(focus1.position, transform.position);
+            Vector3 point = new Vector3(center.x + a * Mathf.Cos(alpha), 0, center.z + b * Mathf.Sin(alpha));
+            rb.MovePosition(Quaternion.Euler(gamma, beta, delta) * point);
+
+
+            deltaAlpha = (Mathf.Sqrt(Mathf.Abs(2 * G * M * (1 / r - 1 / (2 * a))) * 180 * Time.deltaTime) / Mathf.PI * (3 * (a + b) - Mathf.Sqrt((3 * a + b) * (a + 3 * b))));
+
+            alpha += deltaAlpha;
+        }
+        else
+        {
+
+            c = Mathf.Sqrt(Mathf.Abs(b * b - a * a));
+            center = new Vector3(focus1.position.x, 0, focus1.position.z + c);
+
+            r = Vector3.Distance(focus1.position, transform.position);
+            Vector3 point = new Vector3(center.x + a * Mathf.Cos(alpha), 0, center.z + b * Mathf.Sin(alpha));
+            rb.MovePosition(Quaternion.Euler(gamma, beta, delta) * point);
+
+
+            deltaAlpha = (Mathf.Sqrt(Mathf.Abs(2 * G * M * (1 / r - 1 / (2 * b))) * 180 * Time.deltaTime) / Mathf.PI * Mathf.Sqrt(Mathf.Abs((a * a + b * b) / 2)));
+
+            alpha += deltaAlpha;
+        }
+    }
+}
---
+++ VisualizeOrbit.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizeOrbit : MonoBehaviour
+{
+    LineRenderer line;
+    [SerializeField] GameObject body;
+    [SerializeField] float alpha;
+    private float a;
+    private float b;
+    private float c;
+    Vector3 center;
+    public Vector3 apoapsis;
+    public Vector3 periapsis;
+
+    public float distanceFromFocus;
+    [SerializeField] float error;
+    [SerializeField] GameObject ApoSign; //Apoapsis Indicator
+    [SerializeField] GameObject PeriSign; //Periapsis Indicator
+    public float beta;
+    public float gamma;
+    public float delta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        a = body.GetComponent<Orbit>().a;
+        b = body.GetComponent<Orbit>().b;
+        c = body.GetComponent<Orbit>().c;
+        center = body.GetComponent<Orbit>().center;
+        beta = body.GetComponent<Orbit>().beta;
+        gamma = body.GetComponent<Orbit>().gamma;
+        delta = body.GetComponent<Orbit>().delta;
+        body.GetComponent<Orbit>().orb = transform.gameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        a = body.GetComponent<Orbit>().a; //To Update in Real Time (there's a better way!!!)
+        b = body.GetComponent<Orbit>().b;
+        c = body.GetComponent<Orbit>().c;
+        center = body.GetComponent<Orbit>().center;
+        beta = body.GetComponent<Orbit>().beta;
+        gamma = body.GetComponent<Orbit>().gamma;
+        delta = body.GetComponent<Orbit>().delta;
+
+        for (int i=0; i < line.positionCount; i++)
+        {
+            
+            alpha = alpha + 360 / (line.positionCount -1) * Mathf.Deg2Rad;
+            //line.SetPosition(i, new Vector3(center.x + a * Mathf.Cos(alpha), 0, center.z + b * Mathf.Sin(alpha)));
+            Vector3 point = new Vector3(center.x + a * Mathf.Cos(alpha), 0, center.z + b * Mathf.Sin(alpha));
+            line.SetPosition(i, Quaternion.Euler(gamma, beta, delta) *point);
+
+            distanceFromFocus = Vector3.Distance(new Vector3(center.x + a * Mathf.Cos(alpha), 0, center.z + b * Mathf.Sin(alpha)), body.GetComponent<Orbit>().focus1.position);
+
+            if (a>=b)
+            {
+                
+                if(distanceFromFocus < a + c + error&& distanceFromFocus > a + c - error)
+                {
+                    apoapsis = Quaternion.Euler(gamma, beta, delta) * point;
+                    ApoSign.transform.position = apoapsis;
+                }
+
+            }
+            else {
+                if (distanceFromFocus < b + c + error && distanceFromFocus > b + c - error)
+                {
+                    apoapsis = Quaternion.Euler(gamma, beta, delta) * point;
+                    ApoSign.transform.position = apoapsis;
+                }
+
+            }
+
+            if (a >= b)
+            {
+                if (distanceFromFocus < a - c + error && distanceFromFocus > a - c - error)
+                {
+                    periapsis = Quaternion.Euler(gamma, beta, delta) * point;
+                    PeriSign.transform.position = periapsis;
+                }
+
+            }
+            else
+            {
+                if (distanceFromFocus < b - c + error && distanceFromFocus > b - c - error)
+                {
+                    periapsis = Quaternion.Euler(gamma, beta, delta) * point;
+                    PeriSign.transform.position = periapsis;
+                }
+
+            }
+
+        }
+        alpha = 0;
+    }
+}
